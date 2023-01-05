bc173681ecef808531375a32e405a982487a4872
Radomir Mijovic <radomir.mijovic@productdna.com>
2023-01-05 20:59:42 +0100

    created first config

---
+++ app/page.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import "../styles/global.css"
+
+const Page = () => {
+    return (
+        <div>
+            Home page
+        </div>
+    );
+};
+
+export default Page;
