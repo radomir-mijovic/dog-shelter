7a526155b2fcc056139e5cb99308dea5e30371d2
Radomir Mijovic <radomir.mijovic@productdna.com>
2023-01-14 14:22:44 +0100

    feat: modified donation modal

--- app/donate-modal.js
+++ app/donate-modal.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import styles from "../styles/DonateModal.module.scss"
+import globalStyles from "../styles/Global.module.scss"
 import {motion} from "framer-motion";
 import Image from "next/image";
-import dogImage from "../public/images/dog5.jpg"
+import piggyBankImage from "../public/images/piggy_bank.webp"
 
 const DonateModal = ({modalHandler}) => {
     return (
@@ -13,7 +14,26 @@ const DonateModal = ({modalHandler}) => {
                 transition={{duration: .3}}
                 className={styles.modal}>
                 <div className={styles.imageContainer}>
-                    <Image src={dogImage} fill alt="dog laying"/>
+                    <Image src={piggyBankImage} fill objectFit={"cover"} alt="dog laying"/>
+                </div>
+                <div className={styles.textContainer}>
+                    <h3 style={{textAlign: "center"}} className={globalStyles.globalH3}>
+                        Hvala Vam sto vjerujete u nas
+                    </h3>
+                    <p style={{textAlign: "start"}} className={globalStyles.globalP}>
+                        Donaciju mozete izvrsiti uplaton na racun u CKB banci
+                    </p>
+                    <ul>
+                        <li className={styles.listItem}>
+                            Broj racuna: 510-0000-00
+                        </li>
+                        <li className={styles.listItem}>
+                            Primalac: Rj Azil Budva
+                        </li>
+                        <li className={styles.listItem}>
+                            Svrha uplate: Donacija
+                        </li>
+                    </ul>
                 </div>
                 <div className={styles.buttonContainer}>
                     <motion.button
